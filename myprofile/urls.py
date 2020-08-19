37e531def80946a00c42ee334704afa12e4da759
Tobi DEGNON <tobidegnon@protonmail.com>
2020-08-19 12:56:30 +0100

    update home page

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -4,5 +4,5 @@ from .views import HomeView, HireMeView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("project-create/", HireMeView.as_view(), name="hire_me"),
+    path("hire-me/", HireMeView.as_view(), name="hire_me"),
 ]
