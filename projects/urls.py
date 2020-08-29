bbfd2c95289377fae4353e70d69021f5940e5908
Tobi DEGNON <tobidegnon@protonmail.com>
2020-08-29 17:10:35 +0100

    update home and project detail page element disposition

--- projects/urls.py
+++ projects/urls.py
@@ -11,7 +11,7 @@ app_name = "projects"
 urlpatterns = [
     path("", ProjectListView.as_view(), name="project_list"),
     path("project-create/", ProjectCreateView.as_view(), name="project_create"),
-    path("project-details/<str:slug>", ProjectDetailView.as_view(), name="project_detail"),
-    path("project-update/<str:slug>", ProjectUpdateView.as_view(), name="project_update"),
-    path("project-delete/<str:slug>", ProjectDeleteView.as_view(), name="project_delete"),
+    path("project-details/<str:slug>/", ProjectDetailView.as_view(), name="project_detail"),
+    path("project-update/<str:slug>/", ProjectUpdateView.as_view(), name="project_update"),
+    path("project-delete/<str:slug>/", ProjectDeleteView.as_view(), name="project_delete"),
 ]
