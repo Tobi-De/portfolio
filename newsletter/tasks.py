f18e2530147a4ab76494b2e2976619f2ff6f18ad
Tobi DEGNON <tobidegnon@protonmail.com>
2020-10-19 22:18:47 +0100

    write functions for montly recap

--- newsletter/tasks.py
+++ newsletter/tasks.py
@@ -1,3 +1,6 @@
+from django.template.loader import render_to_string
+from django.utils import timezone
+
 from blog.models import Post
 from .models import News
 
@@ -10,7 +13,9 @@ def send_news_task(key_identifier):
 
 
 def post_month_recap():
-    # filter by last month post
-    posts = Post.all_published_post()
-    # read message template
-    # create news and send it
+    last_month_datetime = timezone.now() + timezone.timedelta(days=-30)
+    posts = Post.all_published_post().filter(publish_date__gt=last_month_datetime)
+    message = render_to_string(
+        "newsletter/email/monthly_posts_recap.html", context={"posts": posts}
+    )
+    News.objects.create(subject="All posts of the Month", message=message).send()
