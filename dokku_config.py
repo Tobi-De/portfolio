175e6dd4ec2a0ce3408fcdf0e5f5a3eadc980ac8
Tobi DEGNON <tobidegnon@protonmail.com>
2020-12-27 09:55:19 +0100

    update dokku config file

--- dokku_config.py
+++ dokku_config.py
@@ -3,68 +3,52 @@ import os
 import click
 
 
-def run_commands(command, appname, env_dict):
-    #  app_name=app_name, env_name=env_name, env_value=env_value
+def run_commands(command, app, env_dict):
     for env_name, env_value in env_dict.items():
-        os.system(
-            command.format(app_name=appname, env_name=env_name, env_value=env_value)
-        )
-        # print(command.format(app_name=appname, env_name=env_name, env_value=env_value))
+        os.system(command.format(app_name=app, env_name=env_name, env_value=env_value))
 
 
-def read_env_file(file_path):
-    try:
-        open(file_path, "r")
-    except FileNotFoundError:
-        return {}
-    else:
-        with open(file_path, "r") as f:
-            env_dict = {
-                line.strip().split("=")[0]: line.strip().split("=")[1] for line in f
-            }
-            # for line in f:
-            #     env_list = line.strip().split("=")
-            #     env_dict[env_list[0]] = env_list[1]
-        return env_dict
+def read_env_file(env):
+    f = env.readlines()
+    return {line.strip().split("=")[0]: line.strip().split("=")[1] for line in f}
 
 
 @click.command()
 @click.option(
-    "--env",
-    default="prodconfig",
-    help="The file where the env variables should be read from.",
-)
-@click.option(
-    "--appname",
+    "--app",
     default="",
-    help="The name of your app, this is neccessary when your are\
-     using the dokku cli directly instead of the dokku-toolbet wrapper",
+    help="The name of your app, this is neccessary "
+    "when your are using the dokku cli directly instead of the dokku-toolbet wrapper",
 )
-def set_dokku_app_envs(env, appname):
-    """Simple program that set environment variable for app deploy with dokku"""
+@click.argument(
+    "env", type=click.File("r"),
+)
+def set_dokku_app_envs(env, app):
+    """Simple program that set environment variables for a project deploy with dokku
+    on a vps, taking the env file path as argument.
+    """
 
     command = (
-        "dokku config:set {app_name} {env_name}={env_value}"
-        if appname
+        "dokku config:set {app} {env_name}={env_value}"
+        if app
         else "dt config:set {env_name}={env_value}"
     )
 
-    env_dict = read_env_file(file_path=env)
+    env_dict = read_env_file(env=env)
 
     if not env_dict:
         return
 
-    run_commands(command, appname, env_dict)
+    run_commands(command, app, env_dict)
 
     # set random secret key and admin url, comment if not needed
-    defaults = {
-        "DJANGO_SECRET_KEY": "$(openssl rand -base64 64",
+    extra = {
+        "DJANGO_SECRET_KEY": "$(openssl rand -base64 64 | tr -dc 'A-HJ-NP-Za-km-z2-9')",
         "DJANGO_ADMIN_URL": "$(openssl rand -base64 4096 | tr -dc 'A-HJ-NP-Za-km-z2-9' | head -c 32)/",
-        "WEB_CONCURRENCY": "4",
         "PYTHONHASHSEED": "random",
     }
 
-    run_commands(command, appname, defaults)
+    run_commands(command=command, app=app, env_dict=extra)
 
 
 if __name__ == "__main__":
